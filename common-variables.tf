07bd39089853b84432046c2c29b2ff3656fe066e
HenriqueFerro-Oracle <henrique.ferro@oracle.com>
2021-08-04 19:10:55 -0300

    Create common-variables.tf

---
+++ common-variables.tf
@@ -0,0 +1,11 @@
+#Define application name
+variable "app_name" {
+  type = string
+  description = "Application name"
+  default = "kopicloud"
+}#Define application environment
+variable "app_environment" {
+  type = string
+  description = "Application environment"
+  default = "demo"
+}
